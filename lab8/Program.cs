5450865b787902204c0e664d78811572b4b4f9d4
Ирина_Колясева <irinakolyaseva95@gmail.com>
2021-12-12 16:01:02 +0300

    Добавьте файлы проекта.

---
+++ lab8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string papka = @"E:\учёба Ирины\S# обучение";
+            if (Directory.Exists(papka))
+            {
+                Console.WriteLine("подкаталоги:");
+                string[] papk = Directory.GetFiles(papka);
+                foreach (string s in papk)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                string[] files = Directory.GetFiles(papka);
+                foreach (string s in files)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ lab8_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            string path = @"E:\учёба Ирины\S# обучение\lab8.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            using (StreamWriter ch = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int chislo = Convert.ToInt32(rand.Next(10));
+                    ch.WriteLine(chislo);
+                    Console.WriteLine(chislo);
+                }
+
+            }
+            double Sum= 0;
+            string[] strNum;
+            using (StreamReader strArray = new StreamReader(path))
+            {
+                while (!strArray.EndOfStream)
+                {
+                    strNum = strArray.ReadLine().Split();
+                    foreach (string a in strNum)
+                    {
+                        if (a != null)
+                        {
+                            Sum += double.Parse(a);
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Сумма ={0} ", Sum.ToString());
+            Console.ReadKey();
+        }
+    }
+}
